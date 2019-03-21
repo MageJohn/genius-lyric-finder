2bd1e952cb98d7963a3ee4bb2cc79cfe2fb7a93d
Yuri Pieters <magejohnyjtp@gmail.com>
2019-03-21 21:37:14 +0000

    Initial commit

---
+++ genius-lyric-finder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import os.path
+import lyricsgenius
+
+ACCESS_TOKEN = "access_token.txt"
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), ACCESS_TOKEN)) \
+            as token_file:
+        genius = lyricsgenius.Genius(token_file.readline().strip())
+except FileNotFoundError:
+    print("Couldn't find access token file {}".format(ACCESS_TOKEN))
+    sys.exit(1)
+
+genius.verbose = False
+genius.remove_section_headers = False
+
+if len(sys.argv) > 1:
+    artist = sys.argv[1].replace('_', ' ')
+else:
+    print("No artist passed")
+    sys.exit(2)
+
+if len(sys.argv) > 2:
+    title = sys.argv[2].replace('_', ' ')
+else:
+    print("No song title passed")
+    sys.exit(3)
+
+song = genius.search_song(title, artist)
+
+if song:
+    print(song.lyrics)
+else:
+    sys.exit(4)
+
+sys.exit(0)
